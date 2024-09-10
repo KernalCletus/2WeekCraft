57cc2f75de275d032e86491f7937ec0df996fb6e
KernalCletus <154550761+KernalCletus@users.noreply.github.com>
2024-09-10 12:04:28 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function rickRoll() {
     location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 }
+
+function tutorial() {
+    location.href = 'https://www.youtube.com/watch?v=0vIhBWaBDig'
+}
